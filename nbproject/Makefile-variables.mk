f026cbf2eea7a360df39ded6468637bcf4d0fc91
Paul Warmuth <vespian@gmail.com>
2021-03-25 16:55:19 -0500

    Commit Project Files

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Smart_Shoe_Project.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Smart_Shoe_Project.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=smartshoeproject.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/smartshoeproject.x.tar
